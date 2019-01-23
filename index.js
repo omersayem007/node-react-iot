cfb39afbcba5744bc9bdeb31b28904f9b190bb73
omersayem007 <omer_sayem@yahoo.com>
2019-01-23 16:51:11 +0600

    IR added

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 var five = require("johnny-five");
 var board = new five.Board();
+var controller = process.argv[2] || "GP2Y0A02YK0F";
 
 board.on("ready", function() {
+  var led = new five.Led(13);
 
-  // Create a new generic sensor instance for
-  // a sensor connected to an analog (ADC) pin
-  var sensor = new five.Sensor("A0");
+  this.repl.inject({
+    led: led
+  });
 
-  // When the sensor value changes, log the value
-  sensor.on("change", function(value) {
-    console.log("Temparature"+value)
+  
+
+  var proximity = new five.Proximity({
+    controller: controller,
+    pin: "A0"
+  });
+
+  proximity.on("data", function() {
+
+    if(this.inches > 210 ){
+     // console.log("inches: ", this.inches);
+     led.on();
+     console.log("object is close ");
+    }else{
+      //console.log("cm: ", this.cm);
+      led.off();
+    }
+    
   });
 });
